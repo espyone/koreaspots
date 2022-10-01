d04088c0bdc9ed339b5859dc813f2a950646d7b3
espyone <42947861+espyone@users.noreply.github.com>
2022-10-01 13:04:48 -0500

    Add files via upload

--- korea/korea.js
+++ korea/korea.js
@@ -1,17 +1,17 @@
 const koreaSpots = [
-    {venue: 'Gwangmyeong Cave',	compass: 'W',	city: 'Gwangmyeong', province: 'Gyeonggi',	info: 'cave theme park', category: '',	korean:'광명동굴'},
+    {venue: 'Gwangmyeong Cave',	compass: 'W',	city: 'Gwangmyeong', province: 'Gyeonggi',	info: 'cave theme park', category: 'green',	korean:'광명동굴'},
     {venue: 'Songdo Monster VR',	compass: 'W',	city: 'Incheon', province: 'Gyeonggi',	info: 'VR arcade', category: '',	korean:'송도 몬스터 VR'},
     {venue: 'Riot Skateshop',	compass: 'W',	city: 'Bupyeong, Incheon', province: 'Gyeonggi',	info: 'skate shop', category: '',	korean:'라이엇'},
     {venue: 'Songdo Tri-Bowl',	compass: 'W',	city: 'Incheon', province: 'Gyeonggi',	info: 'art attraction', category: '',	korean:'트라이보울'},
-    {venue: 'Aramaru Observatory',	compass: 'W',	city: 'Incheon', province: 'Gyeonggi',	info: 'bridge with a view', category: '',	korean:'아라마루전망대'},
-    {venue: 'Ara Canal',	compass: 'W',	city: 'Incheon', province: 'Gyeonggi',	info:'', 	korean:'경인아라뱃길'},
+    {venue: 'Aramaru Observatory',	compass: 'W',	city: 'Incheon', province: 'Gyeonggi',	info: 'bridge with a view', category: 'green',	korean:'아라마루전망대'},
+    {venue: 'Ara Canal',	compass: 'W',	city: 'Incheon', province: 'Gyeonggi',	info:'', category:'green',	korean:'경인아라뱃길'},
     {venue: 'Seoul Grand Park',	compass: 'W',	city: 'Gwacheon', province: 'Gyeonggi',	info: 'zoo etc near seoul land', category: '',	korean:'서울대공원분수광장'},
     {venue: 'Seoul Land',	compass: 'W',	city: 'Gwacheon', province: 'Gyeonggi',	info: 'amusement park', category: '',	korean:'서울랜드'},
     {venue: 'Manseok Park X Arcade',	compass: 'SW',	city: 'Suwon', province: 'Gyeonggi',	info: 'skate park', category: '',	korean:'만석공원X게임장'},
-    {venue: 'Suwon Hwaeseong Fortress',	compass: 'SW',	city: 'Suwon', province: 'Gyeonggi',	info: 'fortress', category: '',	korean:'수원화성'},
+    {venue: 'Suwon Hwaeseong Fortress',	compass: 'SW',	city: 'Suwon', province: 'Gyeonggi',	info: 'fortress', category: 'green',	korean:'수원화성'},
     {venue: 'Everland',	compass: 'W',	city: 'Yongin', province: 'Gyeonggi',	info: 'amusement park', category: '',	korean:'에버랜드'},
     {venue: 'Daehwa skate park',	compass: ' NW',	city: 'Goyang', province: 'Gyeonggi',	info: 'skate park', category: '',	korean:'일산 대화 레포츠 공원'},
-    {venue: 'Gamaksan Mountain Chulreong Bridge',	compass: 'NW',	city: 'Paju', province: 'Gyeonggi',	info: 'suspension bridge', category: '',	korean:'감악산 출렁다리'},
+    {venue: 'Gamaksan Mountain Chulreong Bridge',	compass: 'NW',	city: 'Paju', province: 'Gyeonggi',	info: 'suspension bridge', category: 'green',	korean:'감악산 출렁다리'},
     {venue: 'Club MODECI',	compass: 'N [W]',	city: 'Mapo, Seoul', province: 'Gyeonggi',	info: 'music venue', category: '',	korean:'클럽 MODECI'},
     {venue: 'Stitches Guesthouse',	compass: 'N [W]',	city: 'Mapo, Seoul', province: 'Gyeonggi',	info: 'hostel', category: '',	korean:'스티치스 게스트하우스'},
     {venue: 'Club Sharp',	compass: 'N [W]',	city: 'Mapo, Seoul', province: 'Gyeonggi',	info: 'music venue', category: '',	korean:'클럽 샤프'},
@@ -35,44 +35,44 @@ const koreaSpots = [
     {venue: 'Board Korea Ttukseom Branch',	compass: 'N [E]',	city: 'Gwangjin, Seoul', province: 'Gyeonggi',	info: 'skate shop', category: '',	korean:'보드코리아 뚝섬점'},
     {venue: 'Savour Skateshop',	compass: 'N [E]',	city: 'Seongdong, Seoul', province: 'Gyeonggi',	info: 'skate shop', category: '',	},
     {venue: 'Korean Stone Art Museum',	compass: 'N [NE]',	city: 'Seongbuk, Seoul', province: 'Gyeonggi',	info: 'art museum', category: '',	korean:'우리옛돌박물관'},
-    {venue: 'Green Pedal Song Joong-Ang',	compass: 'N [NE]',	city: 'Gangbuk, Seoul', province: 'Gyeonggi',	info: 'bicycle shop',	korean:'그린페달송중점'},
+    {venue: 'Green Pedal Song Joong-Ang',	compass: 'N [NE]',	city: 'Gangbuk, Seoul', province: 'Gyeonggi',	info: 'bicycle shop', category:'',	korean:'그린페달송중점'},
     {venue: 'Green Pedal Gangbuk-gu Beon-dong 467-13',	compass: 'N [NE]',	city: 'Gangbuk, Seoul', province: 'Gyeonggi',	info: 'bicycle shop', category: '',	},
-    {venue: 'Wonju Sogeumsan Chulreong Bridge',	compass:'SW',	city:'Wonju',	province:'Gangwon', info:'suspension bridge',	korean:'원주소금산출렁다리'},
-    {venue: 'Byeolmaro Astronomical Observatory',	compass:'S',	city:'Yeongwol',	province:'Gangwon', info:'astro observatory	',korean:'별마로천문대'},
-    {venue: 'Hwanseongul Cave',	compass:'E',	city:'Samcheok',	province:'Gangwon', info:'cave w/ wateralls',	korean:'환선굴'},
-    {venue: 'Grasshopper Cafe',	compass:'SE',	city:'Jeongseon',  	province:'Gangwon', info:'roadside attraction',	korean:'구절리역 정선선'},
-    {venue: 'Phoenix Pyeongchang Snow Park',	compass:'E',	city:'Pyeongchang',	province:'Gangwon', info:'snow park	',korean:'휘닉스평창스노우파크'},
-    {venue: 'Reverty skate park',	compass:'NE',	city:'Yangyang',	province:'Gangwon', info:'skate park',	},
-    {venue: 'Ulsanbawi Rock entrance at Sinheungsa Temple',	compass:'NE',	city:'Sokcho',	province:'Gangwon', info:'mountain hike',	korean:'신흥사'},
-    {venue: 'Beopjusa Temple',	compass:'W',	city:'Boeun-gun',	province:'Chungbuk', info:'buddhist temple',	korean:'법주사'},
-    {venue: 'Chaebiwon jjimjilbang',	compass:'NE',	city:'Chungju',	province:'Chungbuk', info:'jjimjilbang',	korean:'채비원'},
-    {venue: 'Buseoksa Temple Ganeungile',	compass:'NE',	city:'Yeongju',	province:'Gyeongbuk', info:'buddhist temple',	korean:'부석사 가는길에'},
-    {venue: 'Manbulsa Temple',	compass:'SE',	city:'Yeongcheon',	province:'Gyeongbuk', info:'buddhist temple',	korean:'만불사'},
-    {venue: 'Gyeongju World',	compass:'SE',	city:'Gyeongju',	province:'Gyeongbuk', info:'amusement park',	korean:'경주월드'},
-    {venue: 'Bulguksa Temple',	compass:'SE',	city:'Gyeongju',	province:'Gyeongbuk', info:'buddhist temple',	korean:'불국사'},
-    {venue: 'Golgulsa Temple',	compass:'SE',	city:'Gyeongju',	province:'Gyeongbuk', info:'buddhist temple',	korean:'골굴사'},
-    {venue: 'Uri Charcoal Habin Charcoal Indoor Sauna Cave',	compass:'S [W]',	city:'Dalseong, Degu',	province:'Gyeongbuk', info:'jjimjilbang',	korean:'우리참숯하빈숯굴'},
-    {venue: 'Playground  indoor skate park',	compass:'S [E]',	city:'Jung-gu, Daegu',	province:'Gyeongbuk', info:'skate park',	korean:'플레이그라운드스케이트파크'},
-    {venue: 'Gimhae skatepark',	compass:'SE',	city:'Gimhae',	province:'Gyeongnam', info:'skate park',	korean:'김해스케이트파크'},
-    {venue: 'Monopatin skate shop',	compass:'SE [W]',	city:'Sasang-gu, Busan',	province:'Gyeongnam', info:'skate shop',	korean:'모노파틴'},
-    {venue: 'Songdo Marine Cable Car',	compass:'SE [SW]',	city:'Seo-gu, Busan',	province:'Gyeongnam', info:'cable car ride',	korean:'송도해상케이블카'},
-    {venue: 'Seokbulsa Temple',	compass:'SE [NW]',	city:'Buk-gu, Busan',	province:'Gyeongnam', info:'buddhist temple',	korean:'석불사'},
-    {venue: 'Beomeosa Temple',	compass:'SE [NE]',	city:'Geumjeong-gu, Busan',	province:'Gyeongnam', info:'buddhist temple',	korean:'범어사'},
-    {venue: 'Haedong Yonggungsa', compass:'Temple	SE [NE]',	city:'Gijang-gun, Busan',	province:'Gyeongnam', info:'buddhist temple',	korean:'해동용궁사'},
-    {venue: 'Jiwon Public Bath',	compass:'SE [E]',	city:'Dongnae-gu, Busan',	province:'Gyeongnam', info:'jjimjilbang',	korean:'지원탕'},
-    {venue: 'Oryukdo Skywalk',	compass:'SE [SE]',	city:'Nam-gu, Busan',	province:'Gyeongnam', info:'skywalk / glass bridge',	korean:'오륙도스카이워크'},
-    {venue: 'Hotel AquaPalace Water Park & Jjimjilbang',	compass:'SE [SE]',	city:'Suyeong-gu, Busan', province:'Gyeongnam', info:'jjimjilbang',	korean:'호텔아쿠아펠리스 워터파크 앤 찜질방'},
-    {venue: 'Saryangdo Passenger Terminal - Jirisan Mountain',	compass:'S',	city:'Tongyeong',	province:'Gyeongnam', info:'suspension bridge',	korean:'사량도여객선터미널'},
-    {venue: 'Geoje bamboo theme park',	compass:'S',	city:'Geoje',	province:'Gyeongnam', info:'bamboo forest',	korean:'거제맹종죽테마파크'},
-    {venue: 'Geoje botanical garden',	compass:'S',	city:'Geoje',	province:'Gyeongnam', info:'botanical garden',	korean:'거제식물원'},
-    {venue: 'Guryong Village Bamboo Forest',	compass:'N',	city:'Damyang-gun',	province:'Jeonnam', info:'bamboo forest',	korean:'죽녹원'},
-    {venue: 'Hampyeong Expo Park Butterfly Insect Eco Center',	compass:'NW',	city:'Hampyeong-gun',	province:'Jeonnam', info:'insect + reptile zoo',	korean:'함평엑스포공원나비곤충생태관'},
-    {venue: 'Hwasun Unjusa Temple',	compass:'C',	city:'Hwasun-gun',	province:'Jeonnam', info:'buddhist temple',	korean:'화순운주사'},
-    {venue: 'Songgwangsa Temple Guksajeon Hall',	compass:'SE',	city:'Suncheon',	province:'Jeonnam', info:'buddhist temple',	korean:'송광사국사전'},
-    {venue: 'Jeonju Hanok Village',	compass:'N',	city:'Jeonju',	province:'Jeonbuk', info:'old town village',	korean:'전주 한옥마을'},
-    {venue: 'Korean house (Hanguk Jip) bibimap',	compass:'N',	city:'Jeonju',	province:'Jeonbuk', info:'restaurant',	korean:'한국집'},
-    {venue: 'Geumgang Suspension Bridge',	compass:'N',	city:'Jeonju + Wanju-gun',	province:'Jeonbuk', info:'suspension bridge',	korean:'금강구름다리'},
-    {venue: 'Daejeon Museum of Art',	compass:'SE',	city:'Seo-gu, Daejeon',	province:'Chungnam', info:'art museum',	korean:'대전시립미술관'}
+    {venue: 'Wonju Sogeumsan Chulreong Bridge',	compass:'SW',	city:'Wonju',	province:'Gangwon', info:'suspension bridge', category:'green',	korean:'원주소금산출렁다리'},
+    {venue: 'Byeolmaro Astronomical Observatory',	compass:'S',	city:'Yeongwol',	province:'Gangwon', info:'astro observatory	', category:'green',korean:'별마로천문대'},
+    {venue: 'Hwanseongul Cave',	compass:'E',	city:'Samcheok',	province:'Gangwon', info:'cave w/ wateralls', category:'green',	korean:'환선굴'},
+    {venue: 'Grasshopper Cafe',	compass:'SE',	city:'Jeongseon',  	province:'Gangwon', info:'roadside attraction', category:'',	korean:'구절리역 정선선'},
+    {venue: 'Phoenix Pyeongchang Snow Park',	compass:'E',	city:'Pyeongchang',	province:'Gangwon', info:'snow park	', category:'',korean:'휘닉스평창스노우파크'},
+    {venue: 'Reverty skate park',	compass:'NE',	city:'Yangyang',	province:'Gangwon', info:'skate park', category:'',	},
+    {venue: 'Ulsanbawi Rock entrance at Sinheungsa Temple',	compass:'NE',	city:'Sokcho',	province:'Gangwon', info:'mountain hike', category:'green',	korean:'신흥사'},
+    {venue: 'Beopjusa Temple',	compass:'W',	city:'Boeun-gun',	province:'Chungbuk', info:'buddhist temple', category:'',	korean:'법주사'},
+    {venue: 'Chaebiwon jjimjilbang',	compass:'NE',	city:'Chungju',	province:'Chungbuk', info:'jjimjilbang', category:'',	korean:'채비원'},
+    {venue: 'Buseoksa Temple Ganeungile',	compass:'NE',	city:'Yeongju',	province:'Gyeongbuk', info:'buddhist temple', category:'',	korean:'부석사 가는길에'},
+    {venue: 'Manbulsa Temple',	compass:'SE',	city:'Yeongcheon',	province:'Gyeongbuk', info:'buddhist temple', category:'',	korean:'만불사'},
+    {venue: 'Gyeongju World',	compass:'SE',	city:'Gyeongju',	province:'Gyeongbuk', info:'amusement park', category:'',	korean:'경주월드'},
+    {venue: 'Bulguksa Temple',	compass:'SE',	city:'Gyeongju',	province:'Gyeongbuk', info:'buddhist temple', category:'',	korean:'불국사'},
+    {venue: 'Golgulsa Temple',	compass:'SE',	city:'Gyeongju',	province:'Gyeongbuk', info:'buddhist temple', category:'',	korean:'골굴사'},
+    {venue: 'Uri Charcoal Habin Charcoal Indoor Sauna Cave',	compass:'S [W]',	city:'Dalseong, Degu',	province:'Gyeongbuk', info:'jjimjilbang', category:'',	korean:'우리참숯하빈숯굴'},
+    {venue: 'Playground  indoor skate park',	compass:'S [E]',	city:'Jung-gu, Daegu',	province:'Gyeongbuk', info:'skate park', category:'',	korean:'플레이그라운드스케이트파크'},
+    {venue: 'Gimhae skatepark',	compass:'SE',	city:'Gimhae',	province:'Gyeongnam', info:'skate park', category:'',	korean:'김해스케이트파크'},
+    {venue: 'Monopatin skate shop',	compass:'SE [W]',	city:'Sasang-gu, Busan',	province:'Gyeongnam', info:'skate shop', category:'',	korean:'모노파틴'},
+    {venue: 'Songdo Marine Cable Car',	compass:'SE [SW]',	city:'Seo-gu, Busan',	province:'Gyeongnam', info:'cable car ride', category:'green',	korean:'송도해상케이블카'},
+    {venue: 'Seokbulsa Temple',	compass:'SE [NW]',	city:'Buk-gu, Busan',	province:'Gyeongnam', info:'buddhist temple', category:'',	korean:'석불사'},
+    {venue: 'Beomeosa Temple',	compass:'SE [NE]',	city:'Geumjeong-gu, Busan',	province:'Gyeongnam', info:'buddhist temple', category:'',	korean:'범어사'},
+    {venue: 'Haedong Yonggungsa', compass:'Temple	SE [NE]',	city:'Gijang-gun, Busan',	province:'Gyeongnam', info:'buddhist temple', category:'',	korean:'해동용궁사'},
+    {venue: 'Jiwon Public Bath',	compass:'SE [E]',	city:'Dongnae-gu, Busan',	province:'Gyeongnam', info:'jjimjilbang', category:'',	korean:'지원탕'},
+    {venue: 'Oryukdo Skywalk',	compass:'SE [SE]',	city:'Nam-gu, Busan',	province:'Gyeongnam', info:'skywalk / glass bridge', category:'green',	korean:'오륙도스카이워크'},
+    {venue: 'Hotel AquaPalace Water Park & Jjimjilbang',	compass:'SE [SE]',	city:'Suyeong-gu, Busan', province:'Gyeongnam', info:'jjimjilbang', category:'',	korean:'호텔아쿠아펠리스 워터파크 앤 찜질방'},
+    {venue: 'Saryangdo Passenger Terminal - Jirisan Mountain',	compass:'S',	city:'Tongyeong',	province:'Gyeongnam', info:'suspension bridge', category:'green',	korean:'사량도여객선터미널'},
+    {venue: 'Geoje bamboo theme park',	compass:'S',	city:'Geoje',	province:'Gyeongnam', info:'bamboo forest', category:'green',	korean:'거제맹종죽테마파크'},
+    {venue: 'Geoje botanical garden',	compass:'S',	city:'Geoje',	province:'Gyeongnam', info:'botanical garden', category:'green',	korean:'거제식물원'},
+    {venue: 'Guryong Village Bamboo Forest',	compass:'N',	city:'Damyang-gun',	province:'Jeonnam', info:'bamboo forest', category:'green',	korean:'죽녹원'},
+    {venue: 'Hampyeong Expo Park Butterfly Insect Eco Center',	compass:'NW',	city:'Hampyeong-gun',	province:'Jeonnam', info:'insect + reptile zoo', category:'',	korean:'함평엑스포공원나비곤충생태관'},
+    {venue: 'Hwasun Unjusa Temple',	compass:'C',	city:'Hwasun-gun',	province:'Jeonnam', info:'buddhist temple', category:'',	korean:'화순운주사'},
+    {venue: 'Songgwangsa Temple Guksajeon Hall',	compass:'SE',	city:'Suncheon',	province:'Jeonnam', info:'buddhist temple', category:'',	korean:'송광사국사전'},
+    {venue: 'Jeonju Hanok Village',	compass:'N',	city:'Jeonju',	province:'Jeonbuk', info:'old town village', category:'',	korean:'전주 한옥마을'},
+    {venue: 'Korean house (Hanguk Jip) bibimap',	compass:'N',	city:'Jeonju',	province:'Jeonbuk', info:'restaurant', category:'',	korean:'한국집'},
+    {venue: 'Geumgang Suspension Bridge',	compass:'N',	city:'Jeonju + Wanju-gun',	province:'Jeonbuk', info:'suspension bridge', category:'green forest',	korean:'금강구름다리'},
+    {venue: 'Daejeon Museum of Art',	compass:'SE',	city:'Seo-gu, Daejeon',	province:'Chungnam', info:'art museum', category:'',	korean:'대전시립미술관'}
     ]
     const zoosAquariums = koreaSpots.filter(zoo => zoo.info.includes('zoo') || zoo.info.includes('aquarium'))
     const skateParks = koreaSpots.filter(skate => skate.info.includes('skate') && !skate.info.includes('shop'))
@@ -80,7 +80,9 @@ const koreaSpots = [
     const musicVenues = koreaSpots.filter(music => music.info === "music venue")
     const artVenues = koreaSpots.filter(art => art.info.includes('art'))
     const buddhistTemples = koreaSpots.filter(buddhist => buddhist.info.includes('temple'))
-  
+    const amusementParks = koreaSpots.filter(amusement => amusement.info.includes('amusement'))
+    const outdoorsAttractions = koreaSpots.filter(outdoors => outdoors.category.includes('green'))
+    console.log(koreaSpots[3])
  
  
     let provinceChoice
@@ -105,7 +107,7 @@ const koreaSpots = [
         sLabel.setAttribute('for',`${labelNameID}`)
         searchDiv.appendChild(sLabel)
         let sInput = document.createElement('input')
-        sLabel.innerHTML += `<br><b>${titleLine}</b>`
+        sLabel.innerHTML += `<br>${titleLine}`
         sInput.setAttribute('type', 'checkbox')
         sInput.setAttribute('class', 'checkbox')
         sInput.setAttribute('value', 'yes')
@@ -120,6 +122,8 @@ const koreaSpots = [
    searchAdd('musicvenues', "Music Venues")
    searchAdd('artvenues', "Art Venues")
    searchAdd('zoosaquariums', "Zoos & Aquariums")
+   searchAdd('amusementparks', "Amusement Parks")
+   searchAdd('outdoorsattractions', "Outdoors Attractions")
    
    
    const temples = document.querySelector('#temples');
@@ -134,6 +138,10 @@ const koreaSpots = [
    artvenues.addEventListener('change', postArtVenues)
    const zoosaquariums = document.querySelector('#zoosaquariums');
    zoosaquariums.addEventListener('change', postZoosAquariums)
+   const amusementparks = document.querySelector('#amusementparks');
+   amusementparks.addEventListener('change', postAmusementParks)
+   const outdoorsattractions = document.querySelector('#outdoorsattractions');
+   outdoorsattractions.addEventListener('change', postOutdoorsAttractions)
    
 
     function clearDivs(name) {
@@ -172,6 +180,14 @@ const koreaSpots = [
         newDiv.innerHTML = `<b>Music Venues</b><br>`
         listVenues(musicvenues, musicVenues)
     }
+    function postAmusementParks() {
+        let divName  = 'amusementparksdiv'
+        clearDivs(divName)
+        newDiv = document.createElement('div')
+        newDiv.classList.add(divName)
+        newDiv.innerHTML = `<b>Amusement Parks</b><br>`
+        listVenues(amusementparks, amusementParks)
+    }
     function postArtVenues() {
         let divName  = 'artvenuesdiv'
         clearDivs(divName)
@@ -179,6 +195,14 @@ const koreaSpots = [
         newDiv.classList.add(divName)
         newDiv.innerHTML = `<b>Art Venues</b><br>`
         listVenues(artvenues, artVenues)
+    }
+    function postOutdoorsAttractions() {
+        let divName  = 'outdoorsattractionsdiv'
+        clearDivs(divName)
+        newDiv = document.createElement('div')
+        newDiv.classList.add(divName)
+        newDiv.innerHTML = `<b>Outdoors Attractions</b><br>`
+        listVenues(outdoorsattractions, outdoorsAttractions)
     }
     function postZoosAquariums() {
         let divName  = 'zoosaquasdiv'
